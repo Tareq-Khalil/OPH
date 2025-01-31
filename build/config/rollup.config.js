5ad55c5c5a970e73d9eb95b464355cce19bab50e
Tareq-Khalil <tareqkhalil1415@gmail.com>
2025-01-31 15:27:11 +0200

    Update rollup.config.js

--- build/config/rollup.config.js
+++ build/config/rollup.config.js
@@ -6,8 +6,6 @@ const pkg = require('../../package')
 const year = new Date().getFullYear()
 const banner = `/*!
  * AdminLTE v${pkg.version} (${pkg.homepage})
- * Copyright 2014-${year} ${pkg.author}
- * Licensed under MIT (https://github.com/ColorlibHQ/AdminLTE/blob/master/LICENSE)
  */`
 
 module.exports = {
